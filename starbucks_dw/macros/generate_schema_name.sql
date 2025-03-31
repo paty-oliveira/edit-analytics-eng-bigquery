e126104c6fbb4e83d5dc7e38ad4bffab391e7627
paty-oliveira <dev.patricia.oliveira@gmail.com>
2025-03-31 17:31:07 +0100

    Add schema prefix variable

--- starbucks_dw/macros/generate_schema_name.sql
+++ starbucks_dw/macros/generate_schema_name.sql
@@ -1,10 +1,11 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {%- set default_schema = target.schema -%}
+    {%- set custom_prefix = var('schema_prefix') -%}
 
     {%- if custom_schema_name is none -%} {{ default_schema }}
 
-    {%- else -%} {{ custom_schema_name | trim }}
-
+    {%- else -%}
+        {{ custom_prefix | trim ~ '_' ~ custom_schema_name | trim }}
     {%- endif -%}
 
 {%- endmacro %}
