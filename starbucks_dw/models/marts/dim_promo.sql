f838a2bbab302b84a3c46c8539fdbe4465445bad
Patrícia Oliveira <patriciia.mota@gmail.com>
2025-03-31 10:30:16 +0000

    Add dim_promo model

---
+++ starbucks_dw/models/marts/dim_promo.sql
@@ -0,0 +1,31 @@
+{% set surrogate_key_columns = ["promo_id", "channel"] %}
+
+with
+    unnested_channels as (
+        select
+            promo_id,
+            promo_type,
+            difficulty_rank,
+            reward,
+            duration,
+            ingested_at,
+            cast(channel as string) as channel
+        from {{ ref("stg_promos") }}, unnest(channels) as channel
+    ),
+
+    final as (
+        select
+            {{ dbt_utils.generate_surrogate_key(surrogate_key_columns) }}
+            as offer_channel_key,
+            promo_id,
+            promo_type,
+            channel,
+            difficulty_rank,
+            reward,
+            duration,
+            ingested_at
+        from unnested_channels
+    )
+
+select *
+from final
