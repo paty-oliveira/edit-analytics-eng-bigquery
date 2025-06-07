72365c3f656bae21ffa1158548cd5bb86def7a09
telma_costa <telma.costa-ext@compass-group.fr>
2025-06-07 14:58:39 +0100

    update column on stg_customers

--- starbucks_dw/models/staging/stg_customers.sql
+++ starbucks_dw/models/staging/stg_customers.sql
@@ -3,7 +3,7 @@ id as customer_id,
 COALESCE(gender,'N/A') as gender,
 COALESCE(income,0) as income,
 age,
-PARSE_DATE('%Y%m%d', CAST(became_member_on AS STRING)) as became_member_on,
+PARSE_DATE('%Y%m%d', CAST(became_member_on AS STRING)) as subscribed_date,
 current_timestamp() as ingested_at
 FROM {{ source('starbucks', 'customers')}}
 WHERE AGE < 118
