a6b564652cf8e78e8b165e2ad29d187ca6305852
Pedro Rocha <pedrohrp.rocha@gmail.com>
2025-06-07 14:58:43 +0100

    Change in the columns to subscribed_date

--- starbucks_dw/models/staging/stg_customers.sql
+++ starbucks_dw/models/staging/stg_customers.sql
@@ -3,7 +3,7 @@ SELECT
   COALESCE(gender, 'N/A') AS gender,
   COALESCE(income, 0) AS income,
   age,
-  PARSE_DATE('%Y%m%d', CAST(became_member_on AS STRING)) AS became_member_on,
+  PARSE_DATE('%Y%m%d', CAST(became_member_on AS STRING)) AS subscribed_date,
   CURRENT_TIMESTAMP() AS ingested_at
 FROM {{ source('starbucks', 'customers') }}
 WHERE age < 118
