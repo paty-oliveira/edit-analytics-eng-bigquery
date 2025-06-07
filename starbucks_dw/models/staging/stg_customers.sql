760fe1e2f638f15e8769cad9beaab9d29ee65f60
Michel Silva <michel.gomes.silva@gmail.com>
2025-06-07 14:59:07 +0100

    Changed column name in stg_customers

--- starbucks_dw/models/staging/stg_customers.sql
+++ starbucks_dw/models/staging/stg_customers.sql
@@ -2,7 +2,7 @@ SELECT
   id AS customer_id,
   IFNULL(gender, 'N/A') AS gender,
   IFNULL(income, 0) AS income,
-  PARSE_DATE('%Y%m%d', CAST(became_member_on AS STRING)) AS become_member_on,
+  PARSE_DATE('%Y%m%d', CAST(became_member_on AS STRING)) AS subscribed_date,
   age,
   CURRENT_TIMESTAMP() AS ingested_at
 FROM
