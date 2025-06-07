67cca78b3b34e0264a2d61068c5f1f34b89db314
jorgeneves16 <jorgeneves16@gmail.com>
2025-06-07 15:03:26 +0100

    Fixed collumn name to subscribed_date

--- starbucks_dw/models/staging/stg_customers.sql
+++ starbucks_dw/models/staging/stg_customers.sql
@@ -9,7 +9,7 @@ SELECT
     COALESCE(gender, 'N/A') as gender,
     COALESCE(income, 0) as income,
     age,
-    PARSE_DATETIME('%Y%m%d', CAST(became_member_on AS STRING)) as become_member_on,
+    PARSE_DATETIME('%Y%m%d', CAST(became_member_on AS STRING)) as subscribed_date,
     current_timestamp() as ingested_at
 FROM {{ source('starbucks', 'customers') }}
 WHERE  age != 118
