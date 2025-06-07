049e622d47d4c43413329d72d252b1c72b29fd39
MargaridaGomes <margaridamfgomes@gmail.com>
2025-06-07 14:59:23 +0100

    add alterations_stg_customers_and_schema

--- starbucks_dw/models/staging/stg_customers.sql
+++ starbucks_dw/models/staging/stg_customers.sql
@@ -3,7 +3,7 @@ id as customer_id,
 coalesce(gender, 'N/A') as gender,
 coalesce(income, 0) as income,
 age,
-parse_date('%Y%m%d', cast(became_member_on as string)) as become_member_on,
+parse_date('%Y%m%d', cast(became_member_on as string)) as subscribed_date,
 current_timestamp() as ingested_at
 FROM {{ source ('starbucks','customers')}}
 where age != 118
