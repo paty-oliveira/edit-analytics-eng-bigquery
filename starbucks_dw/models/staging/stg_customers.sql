0cd6f24a9bd2ff27b4b7ece7f3d5cd586330e634
Pedro Rocha <pedrohrp.rocha@gmail.com>
2025-06-07 14:26:30 +0100

    Creation of Customers model and documentation

--- starbucks_dw/models/staging/stg_customers.sql
+++ starbucks_dw/models/staging/stg_customers.sql
@@ -1,17 +1,9 @@
-{{ config(enabled=false) }}
-
-select
-    1 as customer_id,
-    "F" as gender,
-    10 as age,
-    0 as income,
-    current_timestamp as subscribed_date,
-    current_timestamp as ingested_at
-union all
-select
-    2 as cutomer_id,
-    "F" as gender,
-    10 as age,
-    0 as income,
-    current_timestamp as subscribed_date,
-    current_timestamp as ingested_at
+SELECT
+  id AS customer_id,
+  COALESCE(gender, 'N/A') AS gender,
+  COALESCE(income, 0) AS income,
+  age,
+  PARSE_DATE('%Y%m%d', CAST(became_member_on AS STRING)) AS became_member_on,
+  CURRENT_TIMESTAMP() AS ingested_at
+FROM {{ source('starbucks', 'customers') }}
+WHERE age < 118
