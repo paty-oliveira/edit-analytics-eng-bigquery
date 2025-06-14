a3e8cac57fbe9abbc4e1836b680de2c72989d466
tomassalcedas <tomas.salcedas@gmail.com>
2025-06-14 09:59:34 +0100

    dim_date added

--- starbucks_dw/models/marts/dim_date.sql
+++ starbucks_dw/models/marts/dim_date.sql
@@ -1,5 +1,9 @@
 {{ config(enabled=false) }}
 
-select 1
-union all
-select 2
+WITH base AS (
+    {{ dbt_date.get_date_dimension("2025-01-01", "2100-12-31") }}
+)
+SELECT
+    *,
+    CURRENT_TIMESTAMP() AS ingested_at
+FROM base
