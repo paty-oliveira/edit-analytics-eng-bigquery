2921c6120663eae40bfb2234be74be63ccbcd559
telma_costa <telma.costa-ext@compass-group.fr>
2025-06-27 18:51:04 +0100

    add new rep tables

--- starbucks_dw/models/reporting/rep_promos_transactions.sql
+++ starbucks_dw/models/reporting/rep_promos_transactions.sql
@@ -1,39 +1,19 @@
-with customer_transaction_status as (
-    select
-        customer_id,
-        sum(case when transaction_status = 'received' then 1 else 0 end) as total_offers_received,
-        sum(case when transaction_status = 'viewed' then 1 else 0 end) as total_offers_viewed,
-        sum(case when transaction_status = 'completed' then 1 else 0 end) as total_offers_completed,
-        sum(case when transaction_status = 'transaction' then 1 else 0 end) as total_transactions
-    from {{ ref("fct_customers_transactions") }}
-    group by 1
+WITH transaction_status as (
+    SELECT
+        promo_channel, promo_type,
+        sum(case when transaction_status = 'received' then 1 else 0 end) as total_promos_received,
+        sum(case when transaction_status = 'viewed' then 1 else 0 end) as total_promos_viewed
+    FROM {{ ref("fct_promos_transactions") }}
+    GROUP BY 1, 2
 )
 
-
-select fct.customer_id, gender, age, income, customer_subscribed_date,
-case when age between 18 and 28 then 'Gen Z'
-     when age between 29 and 44 then 'Gen Y'
-     when age between 45 and 60 then 'Gen X'
-     when age between 61 and 79 then 'Boomers'
-     when age > 79 then 'Seniors'
-end as age_segment,
-
-case
-  when date_diff(current_date(), customer_subscribed_date, year) < 1 then 'Newcomer'
-  when date_diff(current_date(), customer_subscribed_date, year) between 1 and 2 then 'Engaged'
-  when date_diff(current_date(), customer_subscribed_date, year) between 3 and 4 then 'Loyal'
-  when date_diff(current_date(), customer_subscribed_date, year) between 5 and 6 then 'Veteran'
-  when date_diff(current_date(), customer_subscribed_date, year) >= 7 then 'Legacy Member'
-end as subscription_segment,
-case when income < 30 then 'Basic'
-     when income between 30 and 49 then 'Silver'
-     when income between 50 and 79 then 'Gold'
-     when income >= 80 then 'Platinum'
-end as income_segment,
-cts.total_offers_received,
-cts.total_offers_viewed,
-cts.total_offers_completed,
-cts.total_transactions
-FROM {{ ref("fct_customers_transactions") }} fct
-inner JOIN customer_transaction_status cts on cts.customer_id = fct.customer_id
-group by fct.customer_id, gender, age, income, customer_subscribed_date, cts.total_offers_received, cts.total_offers_viewed, cts.total_offers_completed, cts.total_transactions
+SELECT fpt.promo_channel, fpt.promo_type,
+sum(promo_difficulty_rank) as promo_difficulty_rank, sum(promo_duration) as promo_duration,
+sum(promo_reward) as promo_reward, sum(hours_since_start) as hours_since_start,
+sum(days_since_start) as days_since_start,
+total_promos_received,
+total_promos_viewed,
+round((total_promos_viewed/total_promos_received)*100,2) as response_rate
+FROM {{ ref("fct_promos_transactions") }} fpt
+LEFT JOIN transaction_status ts ON ts.promo_channel = fpt.promo_channel AND ts.promo_type = fpt.promo_type
+GROUP BY promo_channel, promo_type, total_promos_received, total_promos_viewed
